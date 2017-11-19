65f6aef215b53b9b6b9df513c7d94533f605fdc2
tutu godfrey <godfrey_tutu@yahoo.com>
2017-11-19 13:04:53 +0100

    add form validation for addevent, addcenter, change-password, forget-password, continue-forget-password pages

--- template/scripts/index.js
+++ template/scripts/index.js
@@ -7,18 +7,20 @@ const newFuncs = new helperfuncs();
 dom_notifier();
 
 function dom_notifier () {
-const ele_object = document.getElementsByClassName('submitForm');
-let ele_id = newFuncs.newEvent(ele_object[0], 'click', submit_ele, ele_object[0]);
+	const submitElementId = ['signup', 'signin']
+const eleObject = document.getElementsByClassName('submitForm');
+
+let ele_id = newFuncs.newEvent(eleObject[0], 'click', getSubmitEle, eleObject[0]);
 }
 
-function submit_ele(ele) {
-	const submitEleId = ele.getAttribute("id");
+function getSubmitEle(ele) {
+//	const submitEleId = ele.getAttribute("id");
   const formElement = newFuncs.getFormElement(ele);
-	if(submitEleId === 'signin'){
-		validation.validateSignin(formElement);
-	} else if (submitEleId === 'signup') {
+//	if(submitEleId === 'signin'){
+//		validation.validateSignin(formElement);
+//	} else if (submitEleId === 'signup') {
 		validation.validateSignup(formElement);
-	}
+//	}
 }		
 
 
