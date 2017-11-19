18571b667c37f4b6f7b0e76f871500e996e19b8e
tutu godfrey <godfrey_tutu@yahoo.com>
2017-11-19 19:33:24 +0100

    add validation for confirm password and valid email

--- scripts/src/validation.js
+++ scripts/src/validation.js
@@ -1,14 +1,17 @@
-import { helperfuncs, changeAttribute }  from './funcs';
-const newFuncs = new helperfuncs();
+import { HelperFuncs, changeAttribute }  from './funcs';
+const newFuncs = new HelperFuncs();
 
 export const  Validation = class {
   constructor () {
   }
-  
+
   validate(formElement) {
-     let validated = true;
-     let radioChecked = true;
+     let allFieldsValidated = true;
+     let radioChecked = false;
+     let passwordArray = [];
+     let emailIsValid = false;
      let eleControls = formElement.elements;
+     const emailRegExp = /\w+@\w+\.(net|com|org)/;
      const regex = /requiredFields/;
 	   for(let elem of eleControls){
        let eleValue;
@@ -16,49 +19,72 @@ export const  Validation = class {
      if(elem.getAttribute('class')){
        const eleClass = elem.getAttribute('class');
        if(eleClass.match(regex)){
-         const eleType = elem.type;
-         // alert(eleType)
-         let eleId
-         if(eleType === 'text'){
-           eleValue = elem.value.trim();
-           if(eleValue === '' || eleValue === ' '){
+        const eleType = elem.type;
+        let eleId
+        if(eleType === 'text'){
+          eleValue = elem.value.trim();
+          if(eleValue === '' || eleValue === ' '){
               changeAttribute([elem, 'class', 'failValidation form-control']);
-              validated = false;
-           } else {
-            //validated
-           }
-         } else if (eleType === 'password'  && elem.value.trim() === '') {
-           validated = false;
-           changeAttribute([elem, 'class', 'failValidation form-control']);
-         } else if (eleType === 'select-one'){
+              allFieldsValidated = false;
+          } else if(elem.getAttribute('name') === 'email'){
+            emailIsValid = emailRegExp.test(elem.value);
+            if(!emailIsValid){
+              changeAttribute([elem, 'class', 'failValidation form-control']);
+              allFieldsValidated = false;
+            };
+          }   
+        } else if (eleType === 'password') {
+           if(elem.value.trim() === ''){
+             changeAttribute([elem, 'class', 'failValidation form-control']);
+             allFieldsValidated = false;
+          }  else {
+             passwordArray.push(elem.value)
+          }
+            
+        } else if (eleType === 'select-one'){
            let eleName = elem.getAttribute('name');
            eleValue = newFuncs.selectValue(eleName);
            if(eleValue === 'select' || eleValue === ''){
-              changeAttribute([elem, 'class', 'failValidation form-control']);
-           }
-         } else if ( eleType === 'radio') {
-           radioName = elem.name;
+             changeAttribute([elem, 'class', 'failValidation form-control']);
+             allFieldsValidated = false;
+          }
+        } else if ( eleType === 'radio' || eleType === 'checkbox') {
+           let radioName = elem.name;
            eleValue = newFuncs.getCheckedValue(elem);
-           if(eleValue === undefined) {
-             radioChecked = false;
-           } else {
+           if(eleValue !== undefined) {
              radioChecked = true;
-           }
-         }
-
-
-
-       }
-     }
-     }
+          } /*else {
+             radioChecked = true;
+           }*/
+          }
+        }
+      }
+    }
     if(radioChecked === false) {
       const radioEle = document.getElementById('radio-div');
-      changeAttribute([radioEle, 'class', 'failValidation form-group']);
+      // bug fix needed here
+      //changeAttribute([radioEle, 'class', 'failValidation form-group']);
+      //allFieldsValidated = false;
     }
-    if(validated === false){
+    
+    if(passwordArray.length > 1){
+      let confirmPassword;
+      if(passwordArray.length === 2){
+        confirmPassword = newFuncs.confirmPasswordValues(passwordArray[0], passwordArray[1])
+      } else if (passwordArray.length === 3) {
+        confirmPassword = newFuncs.confirmPasswordValues(passwordArray[1], passwordArray[2])
+      }
+      if(!confirmPassword){
+        allFieldsValidated = false;
+      } 
+    }
+    if(allFieldsValidated === false){
       const pElement = document.getElementById('validationNotice');
       pElement.style.visibility = 'visible';
+      console.log('some fields require validation')
+    }  else {
+      // proceed with form submittion
+      console.log('all field is validated');
     }
-   }
-  
+  } 
 }
