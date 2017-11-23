5b2c49dbeb94d586718276eb7bc597b4933bb50d
tutu godfrey <godfrey_tutu@yahoo.com>
2017-11-23 12:18:32 +0100

    POST and PUT routes and add and update events

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,9 @@
 import express from 'express';
 // import controllers
  import EventCenterController from './../controllers/eventCenterController';
+ import EventsController from './../controllers/eventController';
  const eventCenter = new EventCenterController();
+ const events = new EventsController();
 
 
  const Routes = class {
@@ -13,10 +15,18 @@ import express from 'express';
 		app.get('/', (req, res) => {
       res.status(200).send('Welcom to Eventmanager');
       });
+
+		// Route controllers for Event Centers
 		app.post('/api/centers', eventCenter.addEventCenter);
 		app.get('/api/centers', eventCenter.getEventCenters);
 		app.get('/api/centers/:centerId', eventCenter.getEventCenter);
 		app.put('/api/centers/:centerId', eventCenter.updateEventCenter);
+
+		// route controllers for events
+		app.post('/api/events', events.addEvent);
+		app.put('/api/events/:eventId', events.updateEvent);
+		app.delete('/api/events/:eventId', events.deleteEvent);
+
 	}
 }
 
