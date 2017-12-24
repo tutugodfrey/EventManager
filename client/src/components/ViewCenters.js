677243e73b2e5cefa2dcdd6dcfe24df0c3bda9da
tutu godfrey <godfrey_tutu@yahoo.com>
2017-12-24 22:10:29 +0100

    make api call to get centers

--- client/src/components/ViewCenters.js
+++ client/src/components/ViewCenters.js
@@ -4,7 +4,32 @@ import actions from './../redux/actions'
 
 
 class ViewCenters extends React.Component {
+  constructor () {
+    super();
+  }
+  
+  getCenter() {
+    const token = this.props.store.getState().userData.token;
+    const headers =  new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('token', token);
+    const options = {
+      method:'GET',
+      headers,
+    }
+
+    fetch('http://localhost:8080/api/centers', options)
+    .then(res => res.json())
+    .then(data => { 
+      console.log(data)
+      // const newState = this.props.store.getState()
+    })
+  }
+  componentWillMount(){ 
+    this.getCenter()
+  }
 render() {
+  console.log(this.props.store.getState())
   return ( 
     <div>
       Show event centers
