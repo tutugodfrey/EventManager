8e098aa681f34934b8f7bc4bffe4dc31d740f7d0
tutu godfrey <godfrey_tutu@yahoo.com>
2018-01-29 09:53:39 +0100

    modify config.json to use_env_variable for db connection

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,13 +1,15 @@
+import dotenv from 'dotenv';
 import fs   from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
+dotenv.config();
 const basename  = path.basename(__filename);
 const env       = process.env.NODE_ENV || 'development';
 const config    = require(path.join(__dirname, '../..', 'config', 'config.json'))[env];
-
+console.log(env);
 const db        = {};
 let sequelize
-if (config.use_env_variable) {
+if (config.use_env_variable) {  
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else { 
   sequelize = new Sequelize(config.database, config.username, config.password, config);
