6173af6a448343fd0888157bccb9f0b4289c8cc5
tutu godfrey <godfrey_tutu@yahoo.com>
2018-03-28 22:31:12 +0100

    begin docs for users

--- server/bin/www.js
+++ server/bin/www.js
@@ -6,4 +6,4 @@ app = app.expressServer();
 const port = parseInt(process.env.PORT)|| 8080;
 const server = http.createServer(app);
 server.listen(port);
-console.log(`server start on port ${port}`)
+console.log(`server started on port ${port}`);
